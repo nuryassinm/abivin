d6462766e1bb858da7df6d2d3c28d80764594aec
Nurhussen <109545660+nuryassinm@users.noreply.github.com>
2024-11-08 06:31:00 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <h2 className="font-extrabold">hello world</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
